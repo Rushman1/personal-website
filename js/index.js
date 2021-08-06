afbf8263bbd9b510a1873ebf6583ad85d1551b53
TimSavageHCPNV <tsavage@imailnv.org>
2021-08-06 09:20:22 -0700

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+(function(baseCode) {
+	baseCode(window.jQuery, window, document);
+}(function($, window, document) {
+	$(function() {
+
+		setFooter();
+		hideWaitSymbol();
+		
+		// Button Clicks
+
+	});
+	// Wrappers
+
+	// AJAX Functions
+
+	// Functions
+
+	// Dynamic Buttons
+}));
