fdaddddee45e26d133b586a02a7f72f46a6a0a28
TimSavageHCPNV <tsavage@imailnv.org>
2021-08-08 22:52:11 -0700

    Added fonts and header styles

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
-'use strict';
-
 (function(baseCode) {
+	'use strict';
 	baseCode(window.jQuery, window, document);
 }(function($, window, document) {
 	$(function() {
 		
 		// Button Clicks
+
+		// var inter = window.setInterval(() => {
+		// 	var left = parseInt($(".red").css('left'));
+		// 	var top = parseInt($(".red").css('top'));
+		// 	const parent = $('.red').parent();
+
+		// 	var parentWidth = parseInt(parent.css('width')) - parseInt($('.red').css('width'));
+		// 	var parentHeight = parseInt(parent.css('height')) - parseInt($('.red').css('height'));
+			
+		// 	var rTop = getRandomInt(parentHeight);
+		// 	var rLeft = getRandomInt(parentWidth);
+
+		// 	setBlockPosition(rTop, rLeft);
+		// }, 500);
+
+		// window.setTimeout(() => {
+		// 	clearInterval(inter);
+		// }, 60000);
 
 	});
 	// Wrappers
@@ -13,6 +30,13 @@
 	// AJAX Functions
 
 	// Functions
-
+	function setBlockPosition(top, left){
+		const block = $(".red");
+		block.css({top: top + 'px', left: left + 'px'});
+		$("#coords").html($("#coords").html() + top + ':' + left + '<br/>');
+	}
+	function getRandomInt(max){
+		return Math.floor(Math.random()*max);
+	}
 	// Dynamic Buttons
 }));
