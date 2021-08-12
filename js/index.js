4a440122c1144e9ae44cbc587a03b7834f98b873
TimSavageHCPNV <tsavage@imailnv.org>
2021-08-12 14:07:55 -0700

    Added background image and further enhancements

--- js/index.js
+++ js/index.js
@@ -6,24 +6,6 @@
 		
 		// Button Clicks
 
-		// var inter = window.setInterval(() => {
-		// 	var left = parseInt($(".red").css('left'));
-		// 	var top = parseInt($(".red").css('top'));
-		// 	const parent = $('.red').parent();
-
-		// 	var parentWidth = parseInt(parent.css('width')) - parseInt($('.red').css('width'));
-		// 	var parentHeight = parseInt(parent.css('height')) - parseInt($('.red').css('height'));
-			
-		// 	var rTop = getRandomInt(parentHeight);
-		// 	var rLeft = getRandomInt(parentWidth);
-
-		// 	setBlockPosition(rTop, rLeft);
-		// }, 500);
-
-		// window.setTimeout(() => {
-		// 	clearInterval(inter);
-		// }, 60000);
-
 	});
 	// Wrappers
 
