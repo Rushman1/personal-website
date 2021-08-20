7e08d3959621e67c75d87a7b80dfe54f59176907
TimSavageHCPNV <tsavage@imailnv.org>
2021-08-20 10:59:05 -0700

    Adding move functions

--- js/index.js
+++ js/index.js
@@ -1,24 +1,63 @@
 (function(baseCode) {
 	'use strict';
-	baseCode(window.jQuery, window, document);
-}(function($, window, document) {
+	baseCode(window.jQuery, window, document, window.utilities);
+}(function($, window, document, utils) {
+	var options = {};
+	var count = 0;
 	$(function() {
 		
-		// Button Clicks
+		window.setTimeout(() => {
+			options = optionList[count];
+			count = ++count;
+			moveBox();
+		}, 5000);
 
+		// Button Clicks
+		$(document).on('click', '#next-button', function(){
+			$(".popover").hide();
+			if(count>=optionList.length) return;
+			options = optionList[count];
+			count = ++count;
+			moveBox();
+		});
 	});
 	// Wrappers
 
 	// AJAX Functions
 
 	// Functions
-	function setBlockPosition(top, left){
-		const block = $(".red");
-		block.css({top: top + 'px', left: left + 'px'});
-		$("#coords").html($("#coords").html() + top + ':' + left + '<br/>');
-	}
 	function getRandomInt(max){
 		return Math.floor(Math.random()*max);
+	}
+
+	function moveBox(){
+		var mvBox = document.getElementById('my-avatar');
+		var popover = new bootstrap.Popover(mvBox, options);
+		var x = $(mvBox).css('left');
+		var y = $(mvBox).css('top');
+		var height = $(mvBox).css('height');
+		var width = $(mvBox).css('width');
+
+		var $testimonials = $("#testimonials");
+		var $testWidth = parseInt($($testimonials).css('width'));
+		var $testHeight = parseInt($($testimonials).css('height'));
+
+		var newX = getRandomInt($testWidth - parseInt(width));
+		var newY = getRandomInt($testHeight - parseInt(height));
+		
+		$(mvBox).animate({
+			left: positions[count-1].x,
+			top: positions[count-1].y
+		},{
+			duration: 400,
+			complete: function(){
+				showPopup(popover);
+			}
+		});
+	}
+
+	function showPopup(popover){
+		popover.show();
 	}
 	// Dynamic Buttons
 }));
---
+++ js/utilities.js
@@ -0,0 +1,63 @@
+var optionList = [
+    {			
+        placement: 'left',
+        html: true,
+        title: 'Hello There!',
+        content: '<div><p>Welcome to Savage Creations Website!</p></div>',
+        template: '<div class="popover" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div><div class="popover-footer"><a id="next-button" class="btn btn-secondary">Next!</a></div></div>'
+    },
+    {
+        placement: 'right',
+        html: true,
+        title: 'Enjoy',
+        content: '<div><p>Please make sure to enjoy all the site has to offer you!</p></div>',
+        template: '<div class="popover" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div><div class="popover-footer"><a id="next-button" class="btn btn-secondary">Next!</a></div></div>'
+    },
+    {
+        html: true,
+        title: 'Projects - <i class="fas fa-globe"></i>',
+        content: '<div><p>You can click here to look at some of the projects that I have created (working in my spare time) <strong><em>(coming)</em><strong></p></div>',
+        template: '<div class="popover" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div><div class="popover-footer"><a id="next-button" class="btn btn-secondary">Next!</a></div></div>'
+    },
+    {
+        html: true,
+        title: 'Recipes - <i class="fas fa-pizza-slice"></i>',
+        content: '<div><p>You can click here to enjoy some recipes that I have gathered from the internet! <strong><em>(coming)</em><strong></p></div>',
+        template: '<div class="popover" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div><div class="popover-footer"><a id="next-button" class="btn btn-secondary">Next!</a></div></div>'
+    },
+    {
+        html: true,
+        title: 'Contact Me!',
+        content: '<div><p>And you can even drop me a line by clicking on the \'Contact\' link and then clicking on the Email link.</p><p>I love suggestions for projects and corrections/better ways to do something</p></div>',
+        template: '<div class="popover" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div><div class="popover-footer"><a id="next-button" class="btn btn-secondary">Next!</a></div></div>'
+    },
+    {
+        html: true,
+        title: 'Goodbye!',
+        content: '<div><p>So now, please enjoy yourselves and let me know how I can make this site better!</p></div>',
+        template: '<div class="popover" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div><div class="popover-footer"><a id="next-button" class="btn btn-secondary">Next!</a></div></div>'
+    }
+];
+
+var positions = [
+    {
+        y: '60px',
+        x: '1200px'
+    },
+    {
+        y: '120px',
+        x: '800px'
+    },
+    {
+        y: '200px',
+        x: '1000px'
+    },
+    {
+        y: '20px',
+        x: '100px'
+    },
+    {
+        y: '75px',
+        x: '500px'
+    }
+];
